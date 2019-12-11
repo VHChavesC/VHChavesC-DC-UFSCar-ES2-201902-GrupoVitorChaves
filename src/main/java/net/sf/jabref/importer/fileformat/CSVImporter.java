d0ba327d2108629b753a46baec8086954ad2a59a
Ariel Semensato <arillsemensato@gmail.com>
2019-12-11 18:08:52 -0300

    Implementação do importador CSV
    Alteração nos arquivos de layout para utilizar um separador arbitrário único (&1515). Implementação do importador CSV

--- src/main/java/net/sf/jabref/importer/fileformat/CSVImporter.java
+++ src/main/java/net/sf/jabref/importer/fileformat/CSVImporter.java
@@ -15,15 +15,14 @@
  */
 package net.sf.jabref.importer.fileformat;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 import net.sf.jabref.importer.ImportFormatReader;
 import net.sf.jabref.importer.OutputPrinter;
-import net.sf.jabref.importer.ParserResult;
+import net.sf.jabref.importer.fileformat.ImportFormat;
 import net.sf.jabref.model.entry.BibEntry;
+import net.sf.jabref.model.entry.BibtexEntryTypes;
 
 /**
  * This importer exists only to enable `--importToOpen someEntry.bib`
@@ -53,8 +52,89 @@ public class CSVImporter extends ImportFormat {
      */
     @Override
     public List<BibEntry> importEntries(InputStream in, OutputPrinter status) throws IOException {
-        ParserResult pr = BibtexParser.parse(ImportFormatReader.getReaderDefaultEncoding(in));
-        return new ArrayList<>(pr.getDatabase().getEntries());
+        //ParserResult pr = BibtexParser.parse(ImportFormatReader.getReaderDefaultEncoding(in));
+        //return new ArrayList<>(pr.getDatabase().getEntries());
+        List<BibEntry> BibItems = new ArrayList<>();
+        BufferedReader BufR = new BufferedReader(ImportFormatReader.getReaderDefaultEncoding(in));
+
+        String[] Campos;
+        String[] CamposHeader;
+
+        //Lê a primeira linha, para decidir as colunas especificadas no arquivo CSV
+        String Linha = BufR.readLine();
+        CamposHeader = Linha.split("&1515");
+        /*int BibEntryT;
+
+        for (BibEntryT = 0; BibEntryT < CamposHeader.length; i++)
+        {
+            if (CamposHeader[BibEntryT] == "BibliographyType")
+                break;
+        }*/
+
+        //Lê as entradas do arquivo
+        Linha = BufR.readLine();
+        while (Linha != null) {
+            if (!Linha.trim().isEmpty()) {
+                Campos = Linha.split("&1515");
+                BibEntry BibE = new BibEntry();
+                BibE.setType(BibtexEntryTypes.TECHREPORT);
+                switch (Campos[0]) {
+                case "Book":
+                    BibE.setType(BibtexEntryTypes.BOOK);
+                    break;
+                case "Booklet":
+                    BibE.setType(BibtexEntryTypes.BOOKLET);
+                    break;
+                case "Proceedings":
+                    BibE.setType(BibtexEntryTypes.PROCEEDINGS);
+                    break;
+                case "Incollection":
+                    BibE.setType(BibtexEntryTypes.INCOLLECTION);
+                    break;
+                case "Inbook":
+                    BibE.setType(BibtexEntryTypes.INBOOK);
+                    break;
+                case "Inproceedings":
+                    BibE.setType(BibtexEntryTypes.INPROCEEDINGS);
+                    break;
+                case "Article":
+                    BibE.setType(BibtexEntryTypes.ARTICLE);
+                    break;
+                case "Manual":
+                    BibE.setType(BibtexEntryTypes.MANUAL);
+                    break;
+                case "Mastersthesis":
+                    BibE.setType(BibtexEntryTypes.MASTERSTHESIS);
+                    break;
+                case "Conference":
+                    BibE.setType(BibtexEntryTypes.CONFERENCE);
+                    break;
+                case "Misc":
+                    BibE.setType(BibtexEntryTypes.MISC);
+                    break;
+                case "Phdthesis":
+                    BibE.setType(BibtexEntryTypes.PHDTHESIS);
+                    break;
+                case "Techreport":
+                    BibE.setType(BibtexEntryTypes.TECHREPORT);
+                    break;
+                case "Unpublished":
+                    BibE.setType(BibtexEntryTypes.UNPUBLISHED);
+                    break;
+                }
+                for (int i = 1; i < CamposHeader.length; i++) {
+                    //Retirar aspas
+                    Campos[i] = Campos[i].replace("\"", "");
+                    //Assigna cada campo ao respectivo header
+                    if (Campos[i] != "") {
+                        BibE.setField(CamposHeader[i], Campos[i]);
+                    }
+                }
+                BibItems.add(BibE);
+                Linha = BufR.readLine();
+            }
+        }
+        return BibItems;
     }
 
     @Override
